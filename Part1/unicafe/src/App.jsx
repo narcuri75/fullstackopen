50f149d6b1a1b057ab3d132e19daf1f57ff99af6
narcuri75 <59380364+narcuri75@users.noreply.github.com>
2025-11-05 15:58:34 -0800

    part1: unicafe 1.10 complete
    part1: unicafe 1.10 complete

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const Button = ({ onClick, children }) => (
+  <button onClick={onClick}>{children}</button>
+)
+
+const StatisticLine = ({ text, value }) => (
+  <div>{text} {value}</div>
+)
+
 const Statistics = ({ good, neutral, bad}) => {
   const total = good + neutral + bad
   const average = total === 0 ? 0 : (good - bad) / total
@@ -9,12 +17,12 @@ const Statistics = ({ good, neutral, bad}) => {
 
   return (
     <div>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {total}</div>
-      <div>average {average}</div>
-      <div>positive {positive} %</div>
+      <StatisticLine text="good" value={good}/>
+      <StatisticLine text="neutral" value={neutral}/>
+      <StatisticLine text="bad" value={bad}/>
+      <StatisticLine text="all" value={total}/>
+      <StatisticLine text="average" value={average}/>
+      <StatisticLine text="positive" value={positive + ' %'}/>
     </div>
   )
 }
@@ -25,16 +33,12 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = good + neutral + bad
-  const average = total === 0 ? 0 : (good - bad) / total
-  const positive = total === 0 ? 0 : (good / total) * 100
-
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={() => setGood(good + 1)}>good</Button>
+      <Button onClick={() => setNeutral(neutral + 1)}>neutral</Button>
+      <Button onClick={() => setBad(bad + 1)}>bad</Button>
 
       <h2>statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad} />
