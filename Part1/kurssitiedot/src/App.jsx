004ba6ee0d732e0f6125d76c3fe16c8c7f93283a
narcuri75 <59380364+narcuri75@users.noreply.github.com>
2025-11-05 15:12:37 -0800

    part1: kurssitiedot 1.1 complete
    started over, folder structure should be better now.

--- Part1/kurssitiedot/src/App.jsx
+++ Part1/kurssitiedot/src/App.jsx
@@ -1,47 +1,19 @@
-//header
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-
-//part
-const Part = (props) => {
-  return <p>{props.name} {props.exercises}</p>
-}
-
-//content
-const Content = (props) => {
-const p = props.parts
-  return (
-    <div>
-      <Part name={p[0].name} exercises={p[0].exercises} />
-      <Part name={p[1].name} exercises={p[1].exercises} />
-      <Part name={p[2].name} exercises={p[2].exercises} />
-      </div>
-  )
-}
-
-//total
-const Total = (props) => {
-const p = props.parts
-  return <p>Number of exercises {p[0].exercises + p[1].exercises + p[2].exercises}</p>
-}
-
-//main
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      { name:'Fundamentals of React', exercises: 10},
-      { name:'Using props to pass data', exercises: 7},
-      { name:'State of a component', exercises: 14}
-    ]
-  }
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>{course}</h1>
+      <p>{part1} {exercises1}</p>
+      <p>{part2} {exercises2}</p>
+      <p>{part3} {exercises3}</p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
