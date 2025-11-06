72d67aac2112447be10fb43c1ce5e1133c5b1192
narcuri75 <59380364+narcuri75@users.noreply.github.com>
2025-11-05 16:25:07 -0800

    part1: anekdootit 1.13 complete
    part1: anekdootit 1.13 complete

--- Part1/anekdootit/src/App.jsx
+++ Part1/anekdootit/src/App.jsx
@@ -13,15 +13,24 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
   const nextAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length);
     setSelected(randomIndex);
   };
 
+  const voteAnecdote = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+  };
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={voteAnecdote}>vote</button>
       <button onClick={nextAnecdote}>next anecdote</button>
     </div>
   );
