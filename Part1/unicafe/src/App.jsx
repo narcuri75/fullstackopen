d409d0ca62d25c94247df1cdb4f4817b1abf0fdc
narcuri75 <59380364+narcuri75@users.noreply.github.com>
2025-11-05 16:05:23 -0800

    part1: unicafe 1.11 complete
    part1: unicafe 1.11 complete

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -5,7 +5,10 @@ const Button = ({ onClick, children }) => (
 )
 
 const StatisticLine = ({ text, value }) => (
-  <div>{text} {value}</div>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
 
 const Statistics = ({ good, neutral, bad}) => {
@@ -16,14 +19,16 @@ const Statistics = ({ good, neutral, bad}) => {
   if (total === 0) return <p>No feedback given</p>
 
   return (
-    <div>
-      <StatisticLine text="good" value={good}/>
-      <StatisticLine text="neutral" value={neutral}/>
-      <StatisticLine text="bad" value={bad}/>
-      <StatisticLine text="all" value={total}/>
-      <StatisticLine text="average" value={average}/>
-      <StatisticLine text="positive" value={positive + ' %'}/>
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good}/>
+        <StatisticLine text="neutral" value={neutral}/>
+        <StatisticLine text="bad" value={bad}/>
+        <StatisticLine text="all" value={total}/>
+        <StatisticLine text="average" value={average}/>
+        <StatisticLine text="positive" value={positive + ' %'}/>
+      </tbody>
+    </table>
   )
 }
 
